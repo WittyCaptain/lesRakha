eab99a527ffadb708e3c7a0869f27f88bad3c5fe
unknown <rakafadilah2009@gmail.com>
2025-06-04 16:22:23 +0700

    Coding 2

---
+++ src/app/components/header.tsx
@@ -0,0 +1,7 @@
+const Header = () =>{
+    return(
+        <h1>HEADERRRRRRRRR</h1>
+    )
+}
+
+export default Header
---
+++ src/pages/home.tsx
@@ -0,0 +1,7 @@
+const home = () =>{
+    return(
+        <h1>Konnichiwa</h1>
+    )
+}
+
+export default home
